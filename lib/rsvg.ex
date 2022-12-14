81b86922aa5d4d3436f21e8e894b3c0b62930ec4
Tallak Tveide <tt@kitemill.no>
2022-12-14 10:35:10 +0100

    Add size option when generating PNG
    This makes the module more generically useful

--- lib/rsvg.ex
+++ lib/rsvg.ex
@@ -4,9 +4,12 @@ defmodule RSVG do
   def load_nifs, do: :erlang.load_nif(:filename.join(:code.priv_dir('rsvg'), 'rsvg'), 0)
 
   @spec render(svg :: binary, format :: :png) :: binary
-  def render(svg, :png) do
+  def render(svg, :png),  do: render(svg, :png, {347, 126})
+
+  @spec render(svg :: binary, format :: :png, {w :: non_neg_integer(), h :: non_neg_integer()}) :: binary
+  def render(svg, :png, {w, h}) do
     # TODO: Extract dimensions from svg
-    nif_render_png(svg, 347, 126)
+    nif_render_png(svg, w, h)
   end
 
   def nif_render_png(_svg, _width, _height), do: raise("NIF nif_render_png/3 not implemented")
